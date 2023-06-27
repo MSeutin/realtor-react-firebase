f4a0d3661261a607436fb7a8c85cc3c6da89490e
Michael Seutin <frenchmike@gmail.com>
2023-06-26 23:58:37 -0700

    added signin, signup, reset password UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import { useLocation, useNavigate } from 'react-router-dom';
 export default function Header() {
     const location = useLocation();
     const navigate = useNavigate();
-    console.log(location);
     function pathMatchRoute(route){
         if(route === location.pathname){
             return true;
