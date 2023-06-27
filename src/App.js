e688fe16178caff46d813c40d1560b5045734666
Michael Seutin <frenchmike@gmail.com>
2023-06-26 17:48:34 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
+import Home from './pages/Home';
+import Offers from './pages/Offers';
+import SignUp from './pages/SignUp';
+import Signin from './pages/Signin';
+import ForgotPassword from './pages/ForgotPassword';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <>
-    <h1 className="text-2xl bg-red-400">hello michael</h1>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/offers" element={<Offers />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/sign-in" element={<Signin />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+    </Router>
     </>
   );
 }
